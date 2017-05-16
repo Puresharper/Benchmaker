5f2ca0bce6f6277503f3910601ae0ed13afeee11
Tony THONG <tony.thong@outlook.com>
2017-05-16 06:05:23 +0200

    -

--- Benchmaker/Benchmaker/Properties/AssemblyInfo.cs
+++ Benchmaker/Benchmaker/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Benchmaker")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Tony THONG")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
