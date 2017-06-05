be09e82ea8d2d127ae8c05f3619afa0505569e04
Tony THONG <tony.thong@outlook.com>
2017-06-05 16:09:51 +0200

    -

--- Benchmaker/Benchmaker/Properties/AssemblyInfo.cs
+++ Benchmaker/Benchmaker/Properties/AssemblyInfo.cs
@@ -1,36 +1,17 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
+using System.Runtime;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Benchmaker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Benchmaker .NET 4.0+ is a lib to help benchmark source code to compare which is more efficient")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tony THONG")]
 [assembly: AssemblyProduct("Benchmaker")]
 [assembly: AssemblyCopyright("Copyright ©  2017 Tony THONG")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Benchmaker")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4a665d24-652a-481a-9a34-335dbcf2d9da")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
